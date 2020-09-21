dabb2ac4ba65414a97ac7ab617ef03a63972e241
Ksenyas21 <56151288+Ksenyas21@users.noreply.github.com>
2020-09-21 09:34:50 +0300

    Lab1

--- 1.1.py
+++ 1.1.py
@@ -6,10 +6,11 @@ x = float(input('x=',))
 a = float(input('a=',))
 e = float(input('e=',))
 k = 1
+Sum =0
 s = math.sin(x)**k+math.cos(a)**k/math.factorial(1*k-1)
 if x != 0 and a != 0:
     while math.fabs(s) > e:
-        s += s
-        k = k+1
-
-print('summa =',s , 'amount arg = ', k)
+        Sum += s
+        k = k + 1
+        s = math.sin(x) ** k + math.cos(a) ** k / math.factorial(2 * k - 1)
+print('summa =',s , 'amount arg = ', k+1)
